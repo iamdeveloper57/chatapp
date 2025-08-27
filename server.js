d315a7d14b968b0937c1b396483a770a06245952
developer <agraharirehan57@gmail.com>
2025-08-27 16:08:46 +0545

    v1

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+
+app.use(cors());
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+  socket.emit("hello", "hello from server!");
+  socket.broadcast.emit("hello", `${socket.id} joined the server`);
+
+  socket.on("message", (data) => {
+    io.emit("re-message", data);
+  });
+
+  // user discounnected
+  socket.on("disconnect", () => {
+    console.log("user Disconected", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("server connected");
+});
