4323ea379d7a43361422b1703f41e60374f50b9e
developer <agraharirehan57@gmail.com>
2025-09-06 20:23:16 +0545

    v4 calling

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", credentials: true },
+  cors: {
+    origin: ["http://localhost:5173", "https://pingbox.netlify.app"],
+    credentials: true,
+  },
 });
 
 connectDB();
